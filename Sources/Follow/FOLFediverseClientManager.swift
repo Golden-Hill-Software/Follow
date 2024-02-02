5f1bb94c72af8ce479082350dc00031ad6994550
John Brayton <jbrayton@goldenhillsoftware.com>
2024-02-02 14:44:48 -0500

    sorted list of fediverse clients

---
+++ Sources/Follow/FOLFediverseClientManager.swift
@@ -0,0 +1,18 @@
+//
+//  FOLFediverseClientManager.swift
+//
+//
+//  Created by John Brayton on 2/2/24.
+//
+
+import Foundation
+
+class FOLFediverseClientManager {
+    
+    let sortedFediverseClients: [FOLFediverseClient] = [
+        FOLIceCubes(),
+        FOLIvory(),
+        FOLMona(),
+    ]
+    
+}
