7567ce0becc8da42a4e359ce6f7934f752d9b282
John Brayton <jbrayton@goldenhillsoftware.com>
2024-02-02 17:47:09 -0500

    Actually populate the menu

--- Sources/Follow/FOLFollow+NSMenuItemValidation.swift
+++ Sources/Follow/FOLFollow+NSMenuItemValidation.swift
@@ -11,7 +11,13 @@ extension FOLFollow : NSMenuItemValidation {
 
     public func validateMenuItem(_ menuItem: NSMenuItem) -> Bool {
         if menuItem.action == #selector(handleFollowOnFediverse(_:)) {
-            
+            if let fediverseProfile = FOLFollow.fediverseProfile {
+                let options = FOLFediverseClientUtilities.options(forFediverseProfile: fediverseProfile)
+                menuItem.submenu = self.menu(forOptions: options, parentMenuItem: menuItem)
+            } else {
+                print("FOLFollow.fediverseProfile is not set.")
+                return false
+            }
         }
         return true
     }
