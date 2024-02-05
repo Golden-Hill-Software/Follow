efa3479c39a3dba64bdb6983d63b9335435b0918
John Brayton <jbrayton@goldenhillsoftware.com>
2024-02-05 11:42:03 -0500

    added some comments around icon options

--- Sources/Follow/FOLOptionIcon.swift
+++ Sources/Follow/FOLOptionIcon.swift
@@ -9,8 +9,16 @@ import Foundation
 
 public enum FOLOptionIcon {
     
+    // Indicates that the option is to open a webpage.
     case web
+
+    // Indicates that the option is to copy a link, handle, or other bit of text to the pasteboard.
     case copy
+    
+    // Indicates that the option is to open a specific application. The application path will always
+    // be nil on iOS. On macOS it can be used to get the icon for the application. The bundleIconName
+    // will always point to an icon (pre-downloaded and cropped via the iTunes API) for the application.
+    // The icons are in Media.assets.
     case applicationIcon(applicationPath: String?, bundleIconName: String)
     
 }
